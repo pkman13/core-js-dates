b163084eab416f13d9c62ea0a0b0176fa76c951d
Sergey <sergey.grinkevich@gmail.com>
2024-03-24 19:54:00 +0200

    feat: done 1-4 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,9 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  const dateObject = new Date(date);
+  return dateObject.getTime();
 }
 
 /**
@@ -31,8 +32,9 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const dateObject = new Date(date);
+  return dateObject.toTimeString().slice(0, 8);
 }
 
 /**
@@ -46,8 +48,18 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const dateObject = new Date(date);
+  const dayName = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  return dayName[dateObject.getUTCDay()];
 }
 
 /**
@@ -61,8 +73,14 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const dateObject = new Date(date);
+  let daysToAdd = 5 - dateObject.getDay();
+  if (daysToAdd <= 0) {
+    daysToAdd += 7;
+  }
+  dateObject.setDate(dateObject.getDate() + daysToAdd);
+  return dateObject;
 }
 
 /**
