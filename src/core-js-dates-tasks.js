0d57dc392fbba4fdd294490b92a5288b90609d4b
Sergey <sergey.grinkevich@gmail.com>
2024-03-25 17:13:56 +0200

    feat: done task 9

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -181,8 +181,16 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const date = new Date(year, month - 1, 1);
+  let sumWeekend = 0;
+  while (date.getMonth() === month - 1) {
+    if (date.getDay() === 0 || date.getDay() === 6) {
+      sumWeekend += 1;
+    }
+    date.setDate(date.getDate() + 1);
+  }
+  return sumWeekend;
 }
 
 /**
@@ -198,8 +206,9 @@ function getCountWeekendsInMonth(/* month, year */) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const dateObject = new Date(date);
+  
 }
 
 /**
