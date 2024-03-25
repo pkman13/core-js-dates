71533e71a533162cbd62b45ba64cf54ecf370b31
Sergey <sergey.grinkevich@gmail.com>
2024-03-25 17:15:51 +0200

    fix: same error

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -206,9 +206,8 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(date) {
-  const dateObject = new Date(date);
-  
+function getWeekNumberByDate(/* date */) {
+  throw new Error('Not implemented');
 }
 
 /**
